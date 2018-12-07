bdb39e94b934dbdf04489ef793b8ce94be1bd45b
Jhosehp Rendon <jhosehp.rendon@gmail.com>
2018-12-07 15:28:49 -0500

    Changes in package

---
+++ build/precache-manifest.bb1d1f55500f491761733640b3d0e063.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6b7bacebd848945cbb5b",
+    "url": "/static/js/main.6b7baceb.chunk.js"
+  },
+  {
+    "revision": "f99f554946b0f76823f8",
+    "url": "/static/js/1.f99f5549.chunk.js"
+  },
+  {
+    "revision": "6b7bacebd848945cbb5b",
+    "url": "/static/css/main.7f78fd8b.chunk.css"
+  },
+  {
+    "revision": "f99f554946b0f76823f8",
+    "url": "/static/css/1.ae2eee16.chunk.css"
+  },
+  {
+    "revision": "c0e27fddfb8a2e80afb9bbcb8a85ca00",
+    "url": "/index.html"
+  }
+];
